045979c193e432c7ccda6f723555d65210a2c56f
crlyw <jsyclyw@gmail.com>
2016-05-03 02:06:23 -0400

    save changes

--- Chapter2/AdalineGD.py
+++ Chapter2/AdalineGD.py
@@ -15,3 +15,16 @@ class AdalineGD(object):
     errors_: list
     
     """
+def __init__(self, eta=0.01, n_iter=50):
+    self.eta = eta
+    self.n_iter - n_iter
+
+def fir(self, X, y):
+    """Fit training data. 
+
+    Parameters
+    ------------
+    X: {array-like}, shape = [n_samples, n_features]
+       Training vectors,
+       where n_samples is the number of samples and n_features is the number of features
+    """
