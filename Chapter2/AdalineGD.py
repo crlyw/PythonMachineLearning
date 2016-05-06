7af03c0b96a07555b28db68017a65c9335733c3b
crlyw <jsyclyw@gmail.com>
2016-05-05 21:53:10 -0400

    save changes

--- Chapter2/AdalineGD.py
+++ Chapter2/AdalineGD.py
@@ -15,16 +15,47 @@ class AdalineGD(object):
     errors_: list
     
     """
-def __init__(self, eta=0.01, n_iter=50):
-    self.eta = eta
-    self.n_iter - n_iter
+    def __init__(self, eta=0.01, n_iter=50):
+        self.eta = eta
+        self.n_iter - n_iter
 
-def fir(self, X, y):
-    """Fit training data. 
+    def fir(self, X, y):
+        """Fit training data. 
 
-    Parameters
-    ------------
-    X: {array-like}, shape = [n_samples, n_features]
-       Training vectors,
-       where n_samples is the number of samples and n_features is the number of features
-    """
+        Parameters
+        ------------
+        X: {array-like}, shape = [n_samples, n_features]
+           Training vectors,
+           where n_samples is the number of samples and 
+           n_features is the number of features.
+        y: array-like, shape = [n_samples]
+           Target values
+
+        Returns
+        ------------
+        self: object
+
+        """
+        self.w_ = np_zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            output = self.net_input(X)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors ** 2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        """Compute linear activation"""
+        return self.net_input(X)
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(X) >= 0.0, 1, -1)
